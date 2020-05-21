42682a2faa1d59a3821a1634e8de9d02ee7cadb4
trpa-reid <rhaefer@trpa.org>
2020-05-21 16:51:40 -0700

    update

--- reference/code/summarizer/runRSummarizer.cmd
+++
@@ -1,7 +0,0 @@
-SET rPath="../R-3.5.1/bin"
-SET codePath="."
-SET run_log="./runSummary.log"
-set scen_name=%1
-
-echo %cd% > %run_log% 2>&1
-%rPath%\Rscript.exe --vanilla --verbose %codePath%/model_summarizer.R %scen_name% >> %run_log% 2>&1
