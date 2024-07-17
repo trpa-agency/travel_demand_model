e908066e2f51218b5ebbb7aaedb202a81b27f434
hannahccarson <40408209+hannahccarson@users.noreply.github.com>
2024-07-17 01:56:27 -0400

    Bike Network/Mode Split/VMT Summarizer

---
+++ 2022/reference/code/summarizer/runVMTVisualizer.cmd
@@ -0,0 +1,7 @@
+call ..\..\..\python_path.bat
+SET codePath="."
+SET run_log="./vmtRunSummary.log"
+set scen_name=%1
+ECHO %PYTHON_PATH% > %run_log% 2>&1
+echo %cd% >> %run_log% 2>&1
+%PYTHON_PATH%\envs\geo_env\python.exe %codePath%/vmt_summarizer.py %scen_name% >> %run_log% 2>&1
